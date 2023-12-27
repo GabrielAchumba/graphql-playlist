9bd47be74b0cb94c7f7afc90034219f901446481
GabrielAchumba <achumba.gabriel@yahoo.com>
2023-12-27 16:36:36 +0100

    updates

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -5,7 +5,8 @@ const {
     GraphQLObjectType, 
     GraphQLString, 
     GraphQLSchema,
-    GraphQLID
+    GraphQLID,
+    GraphQLInt
 } = graphql;
 
 // dummy data
@@ -14,6 +15,12 @@ var books = [
     {name: 'The Final Empire', genre: 'Fantasy', id: '2'},
     {name: 'The Long Earth', genre: 'Sci-Fi', id: '3'},
 ];
+
+var authors = [
+    {name: 'Patric Rothfuss', age: 44, id: '1'},
+    {name: 'Brandon Sanderson', age: 42, id: '2'},
+    {name: 'Terry Pratchett', age: 66, id: '3'}
+]
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
@@ -24,6 +31,17 @@ const BookType = new GraphQLObjectType({
     })
 });
 
+const AuthprType = new GraphQLObjectType({
+    name: 'Author',
+    fields: () => ({
+        id: {type: GraphQLID },
+        name: {type: GraphQLString },
+        age: {type: GraphQLInt }
+    })
+});
+
+
+
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields:{
@@ -33,6 +51,14 @@ const RootQuery = new GraphQLObjectType({
             resolve(parent, args){
                 //code to get data from db /  other source
                 return _.find(books, {id: args.id});
+            }
+        },
+        author:{
+            type: AuthprType,
+            args: {id: {type: GraphQLID }},
+            resolve(parent, args){
+                //code to get data from db /  other source
+                return _.find(authors, {id: args.id});
             }
         }
     }
